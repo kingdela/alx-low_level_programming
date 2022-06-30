c04d52446b7acd4c5553efa1389ba54bdd33f6e0
Kingsley Ohene <kingdela1@gmail.com>
2022-06-30 22:00:16 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *programming_puzzle - store the string Programming is like building a
+ *multilingual puzzle
+ *puts function - prints the string stored in the variable programming_puzzle
+ *with a new line.
+ */
 
 int main()
 {
