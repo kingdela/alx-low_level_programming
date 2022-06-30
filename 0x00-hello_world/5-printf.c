1b94bba5d09c5f999b457735a608290552a44dce
Kingsley Ohene <kingdela1@gmail.com>
2022-06-30 23:47:37 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char p_gramma[70] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf('%d', p_gramma);
+
+	return (0);
+}
