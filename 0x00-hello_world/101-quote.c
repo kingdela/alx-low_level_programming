4aa449c9f4cd162848ecaeb3a2a352e75e0e4b37
Kingsley Ohene <kingdela1@gmail.com>
2022-07-01 04:08:31 +0000

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 /**
@@ -8,8 +9,7 @@
   */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - \
-			Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
