0b6032fcae99280b1af1e292bdd4b438ca23f0ec
Kingsley Ohene <kingdela1@gmail.com>
2022-07-01 19:33:54 +0000

    Printing of positive and negative number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 	/*your code goes there */
 	if (n > 0)
 	{
-		printf("%d is a positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is a negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 		printf("%d is zero\n", n);
