0b9fb4e05ed77b06fc6092c8c8cf75e0bbe1cb9b
Kingsley Ohene <kingdela1@gmail.com>
2022-07-05 21:05:11 +0000

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char n = 1;
+
+	while (n <= 1)
+	{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		n = n + 1;	
+	}
+
+	return (0);
+}
