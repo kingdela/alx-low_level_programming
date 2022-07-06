b6f4885baa6ed6f90cfe0e511ca0a43df3dccd7e
Kingsley Ohene <kingdela1@gmail.com>
2022-07-06 01:31:45 +0000

    correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-  * main - 
+  * print_alphabet - Print alphabets
   *
-  *Return: Always 0 (Success)
+  * Return: Always 0 (Success)
   */
 void print_alphabet(void)
 {
 	char n;
-	
+
 	for (n = 'a'; n <= 'z'; n++)
 		_putchar(n);
 	_putchar('\n');
