955ce5de2669f024415328f5c830b626ebd89ac6
Kingsley Ohene <kingdela1@gmail.com>
2022-07-08 01:29:43 +0000

    function that prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_triangle -  a function that prints a triangle, followed by a new line.
+  * print_triangle - a function that prints a triangle, followed by a new line
   * @size: An input integer
   * Return: Always 0
   */
