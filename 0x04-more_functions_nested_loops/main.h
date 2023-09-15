0ecb87966a3a985747c92838ba19898d9f61d831
Kingsley Ohene <kingdela1@gmail.com>
2023-09-15 05:27:40 +0000

    more functions and nested loop

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,14 +5,14 @@
 
 int _putchar(char c);
 int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _isdigit(int i);
+int mul(int n, int m);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-void print_square(int size);
+void print_square(int m);
 void print_triangle(int size);
 void print_number(int n);
 
